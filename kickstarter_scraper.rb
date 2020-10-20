76a8d26c5f961b819d7af6facdc828c445a53abf
madelinemc <madeline.coons@gmail.com>
2020-10-19 22:23:17 -0500

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,28 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  #iterate through projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = { #we are converting the title into a symbol because symbols make better hash keys than strings.
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value, #13-16 "grabbing each data points using selectors we've already figured out, and adding them to the projec's hash"
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    } 
+  end
+  projects #return the projects hash
 end
+
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_
